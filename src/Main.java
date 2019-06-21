5ff53ebb70b020f5a5bcd4b9d1414d4c2ab20f3b
ZhongWengunag <1902108184@qq.com>
2019-06-21 17:40:33 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         test();
+        System.out.println("789");
     }
     private static  void test(){
         System.out.println("hahah");
