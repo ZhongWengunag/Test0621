5bfccde79428f418dce375136d77b4280d99afd2
zhongwenguang <1902108184@qq.com>
2019-06-21 17:24:15 +0800

    第一次提交

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        test();
+    }
+    private static  void test(){
+        System.out.println("hahah");
     }
 }
